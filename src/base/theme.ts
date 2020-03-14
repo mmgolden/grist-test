ea2f5622b8d079e93737c4dc793252f452fb3c55
Melinda Golden <info@melindagolden.com>
2020-03-13 22:07:45 -0400

    add: Theme

---
+++ src/base/theme.ts
@@ -0,0 +1,19 @@
+export const theme = {
+  colors: {
+    primary: '#26BE56',
+    primaryFont: '#8A8A8A',
+    secondary: '#4A90E2',
+    tertiary: '#E01067',
+    quaternary: '#FFC10F',
+    headerFont: '#333333',
+    subtleFont: '#cccccc',
+    subtleBackground: '#F7F7F7',
+    white: '#ffffff',
+    error: '#e50000',
+  },
+  fonts: {
+    regular: 'roboto-regular',
+    bold: 'roboto-bold',
+    italic: 'roboto-italic',
+  },
+} as const;
